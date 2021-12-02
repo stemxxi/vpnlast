b252e88e8316d78453400dbcb64784a69d0bc6c3
timur <timurkakhkharov@gmail.com>
2021-12-03 04:25:05 +0500

    key

--- src/js/main.js
+++ src/js/main.js
@@ -119,3 +119,60 @@ $slickEl.slick({
 
 // tabs
 
+
+// Test 
+// Basic initialization is like this:
+// $('.your-class').slick();
+
+// I added some other properties to customize my slider
+// Play around with the numbers and stuff to see
+// how it works.
+// $('.slick-carousel').slick({
+//   infinite: true,
+//   slidesToShow: 3, // Shows a three slides at a time
+//   slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+//   arrows: true, // Adds arrows to sides of slider
+//   dots: true // Adds the dots on the bottom
+ 
+// });
+
+// DEMO
+$(document).ready(function(){
+  $('.carousel').slick({
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    
+    dots:true,
+    centerMode: true,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // centerMode: true,
+
+      }
+
+    }, {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+        infinite: true,
+
+      }
+    },  {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: true,
+        
+      }
+    }]
+  });
+});
+
